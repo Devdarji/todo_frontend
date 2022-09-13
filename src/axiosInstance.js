36e34a9654bbf1ac66bd43ff0330c8c4f216445d
Devdarji <you@example.com>
2022-09-14 02:22:29 +0530

    Car Get API Intigreat

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,12 +1,30 @@
 import axios from "axios";
 
 
-export default axios.create({
-    baseURL: "http://localhost:8006/",
-    headers: {
-        "Content-type": "application/json"
-    }
+
+const todoService = axios.create({
+    baseURL: `${process.env.REACT_APP_TODO_SERVICE}/todo/`,
 });
+
+todoService.interceptors.request.use(
+    (config) => {
+        const tmpConfig = config;
+        tmpConfig.headers["X-CSRFToken"] = localStorage.getItem("csrftoken");
+        return tmpConfig;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+
+
+// export default axios.create({
+//     baseURL: "http://localhost:8006/",
+//     headers: {
+//         "Content-type": "application/json"
+//     }
+// });
 
 // const todoService = axios.create({
 //     baseURL: `${process.env.TODO_APP_SERVICE}/`,
@@ -24,4 +42,4 @@ export default axios.create({
 //     }
 // )
 
-// export { todoService };
+export { todoService };
